d3b53df96a4103e3b7de1737a3957ed263712b3e
sidrasadiq <sidrasadiq163@gmail.com>
2024-10-29 09:59:01 +0500

    title Change

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="styles.css">    
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-<title>Bootstrap demo</title>
+<title>Soulmate</title>
   </head>
 
 
