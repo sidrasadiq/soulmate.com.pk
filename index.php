c7d46d33fc5c1b688039a59a7849e12c9547feed
sidrasadiq <sidrasadiq163@gmail.com>
2024-11-18 17:00:31 +0500

    login and signup links added

--- index.php
+++ index.php
@@ -27,11 +27,11 @@
   <nav class="navbar navbar-expand fixed-top">
     <div class="container">
       <a class="navbar-brand me-auto" href="<?php echo homeURL(); ?>"><img src='assets/images/logo.png'></a>
-      <a href="login.php" class="log-in-button me-3">Log In</a>
+      <a href="https://dashboard.soulmate.com.pk/login.php" class="log-in-button me-3">Log In</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <a href="signup.php" class="sign-up-button">Join Us</a>
+      <a href="https://dashboard.soulmate.com.pk/signup.php" class="sign-up-button">Join Us</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
